42509f9598f59865c217225794222a3f57ac960a
venkat-abhi <venkat.abhishek.1999@gmail.com>
2019-06-26 16:11:42 +0530

    handle sending of the packets

--- src/thread_related.c
+++ src/thread_related.c
@@ -7,7 +7,7 @@
 #include "../include/half_open_scan_tcp.h"
 #include "../include/thread_related.h"
 
-pthread_t g_listener_thread;    
+pthread_t g_listener_thread;
 pthread_t g_scanner_thread;
 
 /**
@@ -25,24 +25,24 @@ void create_thread(enum threadType type)
 	case LISTENER_THREAD:
 		ret_v = pthread_create(&g_listener_thread, NULL, &listener, NULL);
 		if (ret_v == 0) {
-			printf("[*] listener thread created successfully");
+			printf("[*] listener thread created successfully\n");
 		}
 		break;
 
 	case SCANNER_THREAD:
 		ret_v = pthread_create(&g_scanner_thread, NULL, &scanner, NULL);
 		if (ret_v == 0) {
-			printf("[*] hello_broadcaster thread created successfully");
+			printf("[*] hello_broadcaster thread created successfully\n");
 		}
 		break;
 
 	default:
-		printf("[#] Invalid thread type has been requested for creation");
+		printf("[#] Invalid thread type has been requested for creation\n");
 		return;
 	}
 
 	if (ret_v == -1) {
-		printf("pthread_create failed: %d", type);
+		printf("pthread_create failed: %d\n", type);
 		exit(EXIT_FAILURE);
 	}
 }
